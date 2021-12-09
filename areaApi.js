60df5faa8dfef634fed57d815e33f6f1cbdc895e
LORDxisserious <91208047+LORDxisserious@users.noreply.github.com>
2021-12-09 09:48:33 +0630

    complete jS for area

--- areaApi.js
+++ areaApi.js
@@ -1,9 +1,9 @@
 let unit = {
-    "base":"square meter",
-    "rates":{
-        "m2":1,
-        "ft2":10.763910417,
-        "km2":0.000001,
-        "acre":0.0002471054,
+    "base": "square meter",
+    "rates": {
+        "m2": 1,
+        "ft2": 10.7634,
+        "km2": 0.000001,
+        "acre": 0.0002471054,
     }
 };
